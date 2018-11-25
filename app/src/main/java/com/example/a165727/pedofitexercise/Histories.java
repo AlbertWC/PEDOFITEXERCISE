0beec06e9320ff2d3216cbc1b8518afb58a4a486
albert <albertwc0718@gmail.com>
2018-11-25 09:55:35 +0800

    test comment

---
+++ app/src/main/java/com/example/a165727/pedofitexercise/Histories.java
@@ -0,0 +1,13 @@
+package com.example.a165727.pedofitexercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Histories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_histories);
+    }
+}
